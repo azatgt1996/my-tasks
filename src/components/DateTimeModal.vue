13782101cdaefd244f89e91c357fc29f65876734
azatgt1996 <azatgt1996@yandex.ru>
2024-11-28 20:54:28 +0300

    firstDayOfWeek

--- src/components/DateTimeModal.vue
+++ src/components/DateTimeModal.vue
@@ -1,14 +1,17 @@
 <template>
   <ion-modal keep-contents-mounted>
-    <ion-datetime :id v-model="model" max="2100-12-31T00:00:00" :locale="tr._code" hour-cycle="h23" />
+    <ion-datetime :id v-model="model" max="2100-12-31T00:00:00" :locale="tr._code" hour-cycle="h23" :firstDayOfWeek />
   </ion-modal>
 </template>
 
 <script setup>
 import { IonModal, IonDatetime } from '@ionic/vue';
 import { useGlobalStore } from '@/global';
+import { computed } from 'vue';
 
 const { tr } = useGlobalStore()
+
+const firstDayOfWeek = computed(() => new Intl.Locale(tr._code).weekInfo.firstDay ?? 1)
 
 defineProps({ id: String })
 
